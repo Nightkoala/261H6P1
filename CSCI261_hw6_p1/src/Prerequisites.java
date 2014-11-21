0eaa66e0b0d3472d5e978e798eaab4b00135c70a
Derek Brown <djb3718@rit.edu>
2014-11-20 21:02:23 -0500

    Added repo to eclipse and created java file

---
+++ CSCI261_hw6_p1/src/Prerequisites.java
@@ -0,0 +1,23 @@
+/**
+ * Prerequisites.java
+ * 
+ * @author	Derek Brown
+ *
+ * Purpose:	To determine the size of the longest prerequisite chain for a series
+ * 			of courses.
+ */
+
+import java.util.Scanner;
+
+public class Prerequisites {
+
+	// Hidden data members
+	
+	// Constructor
+	
+	// Methods
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end Prerequisites class
